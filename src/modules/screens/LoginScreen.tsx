84f9f1a1f31b523c9523304d77869f598bb50cd1
Laercio Silva <laecio-silva@hotmail.com>
2025-04-02 17:51:19 -0300

    module login

---
+++ src/modules/screens/LoginScreen.tsx
@@ -0,0 +1,11 @@
+import { BackgroundImage } from "../styles/LoginScreens.styles";
+
+const LoginScreen = () => {
+  return (
+    <div>
+      <BackgroundImage src="./background2.webp" />
+    </div>
+  );
+};
+
+export default LoginScreen;
